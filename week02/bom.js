46574490125f9841c1d42ea2af9dde402b769474
mserdarozden <43015687+mserdarozden@users.noreply.github.com>
2024-07-03 23:04:34 +0300

    Update bom.js

--- week02/bom.js
+++ week02/bom.js
@@ -1,13 +1,30 @@
-const input = document.querySelector('#favchap');
+
 const button = document.querySelector('button');
-const list = document.querySelector('#list');
+const input = document.querySelector('#favchap');
+input.focus();
 
-const listItem = document.createElement('li');
-const deleteButton = document.createElement('button');
+button.addEventListener("click", function() {
+    if (input.value.trim() !== "") {
+        
+        const list = document.querySelector('#list');
 
-listItem.textContent = input.value;
-deleteButton.textContent = "❌";
+        const listItem = document.createElement('li');
+        const deleteButton = document.createElement('button');
 
+        listItem.textContent = input.value;
+        deleteButton.textContent = "❌";
 
-listItem.appendChild(deleteButton);
-list.appendChild(listItem);
+        input.value = "";
+
+        listItem.appendChild(deleteButton);
+        list.appendChild(listItem);
+
+        deleteButton.addEventListener("click", function() {
+            list.removeChild(listItem);
+            input.focus();
+        });
+    } else {
+        input.focus();
+    }
+
+});
